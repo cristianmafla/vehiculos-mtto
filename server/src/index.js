323b9454ce4d4d3bc16ec95effbc293b9a6463da
cristianmafla <cristiancaxi@gmail.com>
2019-05-13 17:48:18 -0500

    solucion confilcto de ramas

--- server/src/index.js
+++ server/src/index.js
@@ -11,19 +11,8 @@ server.applyMiddleware({ app })
 
 app.use(appConfig);
 
-<<<<<<< HEAD
 const httpServer = http.createServer(app);
 
 server.installSubscriptionHandlers(httpServer);
 
 httpServer.listen(PORT, () => console.log(`*** SERVER OPEN http://localhost:${PORT}${server.graphqlPath} ***`));
-=======
-const httpServer = http.createServer(app)
-server.installSubscriptionHandlers(httpServer)
-
-httpServer.listen(PORT, () => {
-    console.log(`SERVER ON http://localhost:${PORT}`)
-    console.log(`SERVER_APOLLO ON http://localhost:${PORT}${server.graphqlPath}`)
-    console.log(`Subscriptions ready at ws://localhost:${PORT}${server.subscriptionsPath}`)
-});
->>>>>>> f618249947d166393d71f2af084b231d0b661d46
